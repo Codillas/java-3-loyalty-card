1eba08ffa89870d4b8afb0f641c3a0d91abdeb0e
Nikita Mykhailov <nikmikhailov13@gmail.com>
2026-01-26 18:21:25 +0100

    add ping and health controller

---
+++ src/main/java/com/codillas/loyaltycard/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.codillas.loyaltycard.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth(){
+
+        return ResponseEntity.ok().body("OK");
+    }
+
+}
