804bf9516670ffa073736bdad483580a0c47de76
Nikita Mykhailov <nikmikhailov13@gmail.com>
2026-01-26 17:51:38 +0100

    Initial commit

---
+++ src/main/java/com/codillas/loyaltycard/Java3LoyaltyCardApplication.java
@@ -0,0 +1,13 @@
+package com.codillas.loyaltycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java3LoyaltyCardApplication {
+
+    static void main() {
+        SpringApplication.run(Java3LoyaltyCardApplication.class);
+    }
+
+}
