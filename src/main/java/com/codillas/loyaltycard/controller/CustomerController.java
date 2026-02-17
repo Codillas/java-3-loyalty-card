ef785d86f61edb4df357ee06665153263c660744
Nikita Mykhailov <nikmikhailov13@gmail.com>
2026-02-17 18:39:30 +0100

    add role based access; add default user; fix minor bugs;

--- src/main/java/com/codillas/loyaltycard/controller/CustomerController.java
+++ src/main/java/com/codillas/loyaltycard/controller/CustomerController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +28,7 @@ public class CustomerController {
         this.customerMapper = customerMapper;
     }
 
+    @Secured("ADMIN")
     @GetMapping("/customers")
     public ResponseEntity<List<CustomerDto>> getCustomers() {
         List<Customer> customerList = customerService.getCustomers();
@@ -33,6 +36,7 @@ public class CustomerController {
         return ResponseEntity.ok().body(customerDtoList);
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPER_ADMIN', 'ADMIN') or #customerId.toString() == authentication.name")
     @GetMapping("/customers/{customerId}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID customerId) {
         Customer customer = customerService.getCustomer(customerId);
@@ -40,6 +44,7 @@ public class CustomerController {
         return ResponseEntity.ok().body(customerDto);
     }
 
+    @PreAuthorize("hasAnyAuthority('SUPER_ADMIN', 'ADMIN') or #customerId.toString() == authentication.name")
     @PutMapping("/customers/{customerId}")
     public ResponseEntity<CustomerDto> updateCustomer(
             @PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
@@ -51,12 +56,14 @@ public class CustomerController {
         return ResponseEntity.ok().body(customerMapper.toDto(customer));
     }
 
+    @Secured("ADMIN")
     @PutMapping("/customers/{customerId}/activate")
     public ResponseEntity<CustomerDto> activateCustomer(@PathVariable UUID customerId) {
         Customer customer = customerService.activateCustomer(customerId);
         return ResponseEntity.ok().body(customerMapper.toDto(customer));
     }
 
+    @Secured("ADMIN")
     @PutMapping("/customers/{customerId}/block")
     public ResponseEntity<CustomerDto> blockCustomer(@PathVariable UUID customerId) {
         Customer customer = customerService.blockCustomer(customerId);
